302f00dabd83524d5f76f05873d0b68876692c8a
tappi287 <tapper.stefan@gmail.com>
2021-02-08 18:34:58 +0100

    add vue-flux image slider gallery

--- vue/src/components/Dashboard.vue
+++ vue/src/components/Dashboard.vue
@@ -1,32 +1,56 @@
 <template>
-  <div id="dashboard" v-cloak class="position-relative mb-3 text-left">
-    <b-img class="mr-1 float-left no-pointer" src="@/assets/app_icon.webp"
-           width="24" alt="rFactor 2 logo"></b-img>
-    <h5 class="title">Hello {{ userName }}</h5>
-    <p></p>
+  <div v-cloak id="dashboard" class="position-relative mb-3 text-left">
+    <vue-flux class="img-slider rounded" id="img"
+        ref="slider"
+        :captions="vfCaptions"
+        :images="vfImages"
+        :options="vfOptions"
+        :transitions="vfTransitions"
+        @ready="updateHeight">
 
-    <!-- Shortcuts Play with Preset -->
-    <!--<PresetHandler ref="gf" @makeToast="makeToast" @error="setError"
-                   @presets-ready="presetsReady = true"/>-->
-    <div v-if="gfxPresetsReady" class="text-center mt-4 mb-4">
-      <h5 class="mb-3">Graphics Presets</h5>
-      <template v-for="(preset, idx) in gfxHandler.presets.slice(1)">
-        <b-button :key="idx"
-                  :class="gfxHandler.selectedPresetIdx === idx+1 ? 'active mr-2 ml-2' : 'inactive mr-2 ml-2'"
-                  @click="gfxHandler.selectPreset(preset, true)"
-                  v-b-popover.bottom.hover="preset.desc"
-                  :variant="gfxHandler.selectedPresetIdx === idx+1 ? 'rf-orange' : 'rf-blue'">
-          {{ preset.name }}
-        </b-button>
+      <template v-slot:preloader>
+        <flux-preloader/>
       </template>
-    </div>
-    <!-- Shortcut open Setups Directory -->
 
-    <p></p>
+      <template v-slot:caption>
+        <flux-caption v-slot="captionProps" class="img-caption">
+          <b-link :href="captionProps.caption.url" target="_blank" class="text-white">
+            {{ captionProps.caption.text }}
+          </b-link>
+        </flux-caption>
+      </template>
+    </vue-flux>
+    <div id="img-gradient" class="rounded"></div>
+
+    <!-- Display over image slider -->
+    <div class="p-3" id="top-menu">
+      <b-img alt="rFactor 2 logo" class="mr-1 float-left no-pointer"
+         src="@/assets/app_icon.webp" width="24"></b-img>
+      <h6 class="title mb-0">Hello {{ userName }}</h6>
+
+      <!-- Shortcuts Play with Preset -->
+      <div v-if="gfxPresetsReady" class="text-center mb-5" style="position: relative; top: -.5rem;">
+        <h6 class="title">Graphics Presets</h6>
+        <template v-for="(preset, idx) in gfxHandler.presets.slice(1)">
+          <b-button :key="idx"
+                    v-b-popover.bottom.hover="preset.desc"
+                    :class="gfxHandler.selectedPresetIdx === idx+1 ? 'active mr-2 ml-2' : 'inactive mr-2 ml-2'"
+                    :variant="gfxHandler.selectedPresetIdx === idx+1 ? 'rf-orange' : 'rf-blue'"
+                    @click="gfxHandler.selectPreset(preset, true)">
+            {{ preset.name }}
+          </b-button>
+        </template>
+      </div>
+      <!-- Shortcut open Setups Directory -->
+    </div>
+
+    <div id="spacer" class="no-pointer"></div>
+
     <!-- Server Favourites -->
     <transition name="fade">
-        <ServerBrowser ref="serverBrowser" only-favourites
-                       @make-toast="makeToast" @error="setError" />
+      <ServerBrowser ref="serverBrowser" only-favourites class="mt-3"
+                     @server-browser-ready="browserReady"
+                     @error="setError" @make-toast="makeToast"/>
     </transition>
   </div>
 </template>
@@ -34,44 +58,115 @@
 <script>
 import ServerBrowser from "@/components/ServerBrowser"
 import PresetHandler from "@/components/PresetHandler";
-import { userScreenShots } from "@/main"
+import { VueFlux, FluxCaption, FluxPreloader } from 'vue-flux';
+import {getEelJsonObject, chooseIndex, userScreenShots} from "@/main"
+
+function prepareScreenshots () {
+  let imgList = userScreenShots.slice()
+  let images = []
+  let captions = []
+
+  for (let i = 0; i < userScreenShots.length; i++) {
+    const randomIdx = chooseIndex(imgList)
+    const entry = imgList.splice(randomIdx, 1)[0]
+    images.push(entry[0])
+    captions.push({text: 'Shot by ' + entry[1], url: entry[2]})
+  }
+
+  return {images: images, captions: captions}
+}
 
 export default {
   name: "Dashboard",
   data: function () {
     return {
       userName: 'Driver',
-      userScreenUrls: userScreenShots,
       serverBrowserReady: false,
       gfxPresetsReady: false,
+      vfOptions: { autoplay: true, delay: 12000 },
+      vfImages: [],
+      vfTransitions: [ 'fade', 'kenburn', 'swipe', 'fade' ],
+      vfCaptions: [],
     }
   },
-  props: { gfxHandler: PresetHandler },
+  props: {gfxHandler: PresetHandler},
   methods: {
     makeToast(message, category = 'secondary', title = 'Update', append = true, delay = 8000) {
       this.$emit('make-toast', message, category, title, append, delay)
     },
-    setError: async function (error) { this.$emit('error', error) },
+    getDriver: async function () {
+      let r = await getEelJsonObject(window.eel.get_rf_driver()())
+      if (r === undefined || !r.result) {
+        return
+      }
+      r.result.options.forEach(setting => {
+        if (setting.key === 'Player Nick') {
+          this.userName = setting.value
+        }
+      })
+    },
+    setError: async function (error) {
+      this.$emit('error', error)
+    },
+    browserReady() {
+      // Hack to trigger a resize event
+      window.resizeBy(1, 1)
+    },
+    updateHeight() {
+      this.$nextTick(() => {
+        const imgDiv = document.getElementById('img')
+        const topMenu = document.getElementById('top-menu')
+        if (imgDiv === undefined || topMenu === undefined) { return }
+        const imgHeight = imgDiv.offsetHeight
+        const menuHeight = topMenu.offsetHeight
+        document.getElementById('spacer').style.height = String(imgHeight - menuHeight) + 'px'
+      })
+    },
   },
   created() {
-    // pass
+    const r = prepareScreenshots()
+    this.vfImages = r.images; this.vfCaptions = r.captions
+    this.getDriver()
+    window.onresize = this.updateHeight
   },
   components: {
-    ServerBrowser
+    ServerBrowser,
+    VueFlux,
+    FluxCaption,
+    FluxPreloader,
   },
 }
 </script>
 
 <style scoped>
-  .title { font-weight: 300; }
+.title { font-weight: 300; text-shadow: 1px 1px 2px black; }
 
-  .fade-enter-active, .fade-leave-active {
-    transition: opacity 3s, height 3s;
-  }
-  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-    opacity: 0; height: 0;
-  }
-  .no-pointer { pointer-events:none; }
-  .btn-rf-orange, .btn-rf-blue { font-weight: 300; }
-  .btn-rf-orange.active, .btn-rf-blue.active { font-weight: 400; }
+.fade-enter-active, .fade-leave-active { transition: opacity 3s, height 3s; }
+
+.fade-enter, .fade-leave-to {opacity: 0; height: 0;}
+
+.btn-rf-orange, .btn-rf-blue { font-weight: 300; }
+.btn-rf-orange.active, .btn-rf-blue.active { font-weight: 400; }
+.active, .inactive { box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.8), 0 6px 20px 0 rgba(0, 0, 0, 0.75); }
+
+.no-pointer { pointer-events: none; }
+
+#top-menu { z-index: 10; position: relative; }
+#spacer { z-index: -10; position: relative; }
+.img-slider { position: absolute; z-index: 1; width: 100%; }
+.img-caption { position: absolute; width: 100%; bottom: 0; }
+.img-caption a { line-height: 1.1rem; width: 100%; font-family: Ubuntu, "Segoe UI", sans-serif; }
+#img-gradient {
+  position: absolute;
+  background: linear-gradient(rgba(0,0,0,0.5) 0%, rgba(0,0,0,0) 100%);
+  height: 6rem; width: 100%; z-index: 2;
+}
+</style>
+
+<style>
+.flux-image { border-radius: .25rem; }
+.flux-caption {
+  background-color: transparent !important;
+  background: radial-gradient(circle at 50% 250%, rgba(0,0,0, 0.85) 10%, rgba(0,0,0,0) 30%);
+}
 </style>
