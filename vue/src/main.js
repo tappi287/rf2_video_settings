c54c4f977cf63f633262d631f2ef37af03643f4f
tappi287 <tapper.stefan@gmail.com>
2021-02-08 18:40:26 +0100

    add screenshot data for dashboard

--- vue/src/main.js
+++ vue/src/main.js
@@ -16,16 +16,37 @@ Vue.config.productionTip = false
 
 export const settingsAreaId = 'settings-area'
 export const userScreenShots =
-    ['https://steamuserimages-a.akamaihd.net/ugc/778482308449119648/AF3A15572629B5E3D3E6CA8F61F1D22D00CC015F/',
-     'https://steamuserimages-a.akamaihd.net/ugc/784111684599129210/9082347712B91CD634968AFFBBFD93F28FECD32D/',
-     'https://i.imgur.com/vosgbaj.jpg',
-     'https://steamuserimages-a.akamaihd.net/ugc/782979273624056824/156BBD766A6FA08774365288C6ACB85CB78DD34C/',
-     'https://steamuserimages-a.akamaihd.net/ugc/81465294112521264/7DF6076CE337ECAD339F8A87641BD595B508862F/?imw=1920&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
-     'https://steamuserimages-a.akamaihd.net/ugc/1651096185421784231/0627A7B634416946B1E312B1F6137310F42E47E4/',
-     'https://steamuserimages-a.akamaihd.net/ugc/773987969333324412/899168BBB83DE14E0C7442B7D32B2C5532ECA60A/',
-     'https://forum.studio-397.com/index.php?attachments/grab_2782-jpg.24090/',
-     'https://forum.studio-397.com/index.php?attachments/grab_360-jpg.27884/',
-     'https://forum.studio-397.com/index.php?attachments/mc-laren-gtr-senna_06-jpg.27691/'
+    [
+ ['https://forum.studio-397.com/index.php?attachments/porsche_911_gt3_r_2018_01-jpg.27876/', 'philmcqueen',
+  'https://forum.studio-397.com/index.php?threads/community-screenshots-thread-unedited-screens-only.40609/page-320'],
+ ['https://forum.studio-397.com/index.php?attachments/grab_360-jpg.27884/', 'juanchioooo',
+  'https://forum.studio-397.com/index.php?threads/community-screenshots-thread-unedited-screens-only.40609/page-320'],
+ ['https://forum.studio-397.com/index.php?attachments/20200113191349_1-jpg.28772/', 'datasting',
+  'https://forum.studio-397.com/index.php?threads/community-screenshots-thread-unedited-screens-only.40609/page-324'],
+ ['https://forum.studio-397.com/index.php?attachments/365960_20200322152429_1-png.30174/', 'Frank Murphy',
+ 'https://forum.studio-397.com/index.php?threads/community-screenshots-thread-unedited-screens-only.40609/page-331'],
+ ['https://i.imgur.com/3RdME7Q.jpg', 'jayarrbee36',
+  'https://forum.studio-397.com/index.php?threads/community-screenshots-thread-unedited-screens-only.40609/page-334'],
+ ['https://forum.studio-397.com/index.php?attachments/20200521225453_1-jpg.31126/', 'Pocisk',
+  'https://forum.studio-397.com/index.php?threads/community-screenshots-thread-unedited-screens-only.40609/page-334'],
+ ['https://i.ibb.co/Jjtws8M/r-Factor2-2020-05-30-20-32-16.jpg', 'GeraArg',
+  'https://forum.studio-397.com/index.php?threads/community-screenshots-thread-unedited-screens-only.40609/page-335'],
+ ['https://forum.studio-397.com/index.php?attachments/grab_002-jpg.32749/', 'memoNo1',
+  'https://forum.studio-397.com/index.php?threads/community-screenshots-thread-unedited-screens-only.40609/page-341'],
+ ['https://forum.studio-397.com/index.php?attachments/ec551ad7-aedd-487c-8d8b-494a01f445a3-jpeg.34232/', 'M-Bimmer',
+  'https://forum.studio-397.com/index.php?threads/community-screenshots-thread-unedited-screens-only.40609/page-348'],
+ ['https://forum.studio-397.com/index.php?attachments/20201108231324_1-jpg.34446/', 'svictor',
+  'https://forum.studio-397.com/index.php?threads/community-screenshots-thread-unedited-screens-only.40609/page-349'],
+ ['https://forum.studio-397.com/index.php?attachments/365960_20201110141049_1-png.34504/', 'marciovs28',
+  'https://forum.studio-397.com/index.php?threads/community-screenshots-thread-unedited-screens-only.40609/page-349'],
+ ['https://live.staticflickr.com/65535/50629547207_b7a30fef7c_b.jpg', 'Franck',
+  'https://forum.studio-397.com/index.php?threads/community-screenshots-thread-unedited-screens-only.40609/page-351'],
+ ['https://i.imgur.com/CwYk11v.jpg', 'mantasisg',
+  'https://forum.studio-397.com/index.php?threads/community-screenshots-thread-unedited-screens-only.40609/page-352'],
+ ['https://forum.studio-397.com/index.php?attachments/20201221185422_1-jpg.35356/', 'svictor',
+  'https://forum.studio-397.com/index.php?threads/community-screenshots-thread-unedited-screens-only.40609/page-354#post-1050632'],
+['https://forum.studio-397.com/index.php?attachments/2021-02-02-20-40-png.36838/', 'MiguelVallejo',
+ 'https://forum.studio-397.com/index.php?threads/community-screenshots-thread-unedited-screens-only.40609/page-359#post-1059084']
     ]
 
 new Vue({
@@ -39,6 +60,10 @@ export async function getEelJsonObject (promise) {
 
 export function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+export function chooseIndex(choices) {
+  return Math.floor(Math.random() * choices.length);
 }
 
 export var isValid=(function(){
