7162af3d8add1592907e54de7f5c9f02a558c4f2
tappi287 <tapper.stefan@gmail.com>
2022-10-25 22:08:57 +0200

    remove dev proxy

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -2,9 +2,11 @@ const path = require('path');
 
 module.exports = {
   outputDir: "../web", // or whatever you would like your output directory to be, which will be used in the next step.
+  /*
   devServer: {
     proxy: 'https://www.youtube.com/',
   },
+  */
   chainWebpack: config => {
     config.resolve.alias.set(
       'vue$',
